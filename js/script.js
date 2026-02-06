1cab37bc4e40f6316a492461d02bdfbc40854935
antonioerh <antoniofoerh@gmail.com>
2026-02-06 16:34:52 -0300

    Add Google Search missing features

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
+function googleSearch() {
+    event.preventDefault();
+
+    // Store user's input
+    let input = document.getElementById("floatingInput").value;
+
+    // Redirect user to URL
+    let url = "https://google.com/search?q=" + input;
+    window.location.href = url;
+}
+
+function feelingLucky() {
+    event.preventDefault();
+
+    // Store user's input
+    let input = document.getElementById("floatingInput").value;
+
+    // Redirect user to URL
+    let url = "https://google.com/search?q=" + input + "&btnI=1";
+    window.location.href = url;
+}
+
 function imageSearch() {
-    event.preventDefault()
+    event.preventDefault();
 
     // Store user's input
     let input = document.getElementById("floatingInput").value;
@@ -9,7 +31,7 @@ function imageSearch() {
     window.location.href = url;
 }
 function advancedSearch() {
-    event.preventDefault()
+    event.preventDefault();
 
     // Store user's input
     let all = document.getElementById("all").value;
@@ -62,10 +84,10 @@ function advancedSearch() {
     // Populate query for "None of these words"
     for (let i = 0; i < noneWords.length; i++) {
         if (noneInput == "as_eq=") {
-            noneInput += "-" + noneWords[i];
+            noneInput += noneWords[i];
         }
         else {
-            noneInput += "+" + "-" + noneWords[i];
+            noneInput += "+" + noneWords[i];
         }
     }
 
@@ -73,59 +95,3 @@ function advancedSearch() {
     let url = "https://www.google.com/search?" + allInput + "&" + disInput + "&" + anyInput + "&" + noneInput;
     window.location.href = url;
 }
-
-/* function advancedSearch() {
-    event.preventDefault()
-
-    let all = document.getElementById("all").value;
-    let dis = document.getElementById("dis").value;
-    let any = document.getElementById("any").value;
-    let none = document.getElementById("none").value;
-    let pageContent = page.content;
-    let url = "https://www.google.com/search?"
-
-    let allWords = all.split(" ");
-    let disWords = all.split(" ");
-    let anyWords = any.split(" ");
-    let noneWords = none.split(" ");
-
-    let allInput = "as_q="
-    let disInput = "as_epq="
-    let anyInput = "as_oq="
-
-    for (let i = 0; i < allWords.length; i++) {
-        if (!pageContent.includes(allWords[i])) {
-            return false;
-        }
-        else if (allInput == "as_q=") {
-            allInput += allWords[i];
-        }
-        else {
-            allInput += "+" + allWords[i];
-        }
-    }
-
-    if (!pageContent.includes(dis)) {
-        return false;
-    }
-    
-
-    for (let i = 0; i < anyWords.length; i++) {
-        if (pageContent.includes(anyWords[i])) {
-            if (anyInput == "as_oq=") {
-                anyInput += anyWords[i];
-            }
-            else {
-                anyInput += "+" + anyWords[i];
-            }
-        }
-    }
-
-    for (let i = 0; i < noneWords.length; i++) {
-        if (pageContent.includes(noneWords[i])) {
-            return false;
-        }
-
-    }
-}
-*/
